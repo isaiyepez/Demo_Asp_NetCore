2338833de8531552355287bc4305b043a6af5573
isai.yepez <isai.yepez@REM-LT-221031D>
2022-11-07 16:55:24 -0600

    Add project files.

---
+++ Demo_Asp_NetCore/Controllers/WarehouseController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Demo_Asp_NetCore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WarehouseController : ControllerBase
+    {
+        public WarehouseController()
+        {
+
+        }
+
+        [HttpGet("GetWarehouse")]
+        public IActionResult GetWarehouse()
+        {
+            return Ok("Hello World!");
+        }
+
+        //Es recomendable agregar "From query" decorator por legibilidad
+        [HttpGet("GetWarehouseFromQuery")]
+        public IActionResult GetWarehouseFromQuery([FromQuery] int userId, [FromQuery] string userName)
+        {
+            return Ok($"Hello user ID {userId}, and username {userName}");
+        }
+
+        [HttpGet("GetWarehouseFromRoute/{warehouseId}/{warehouseName}")]
+        public IActionResult GetWarehouseFromRoute([FromRoute] int warehouseId, [FromRoute] string warehouseName)
+        {
+            return Ok($"Hello user id from route {warehouseId}, wharehouse {warehouseName}");
+        }
+    }
+}
